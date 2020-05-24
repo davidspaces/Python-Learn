2ddeb8fc75e4bf3886cead24b77b4e21865d4223
David S <davidpsoudamalla@gmail.com>
2020-05-23 19:11:39 -0700

    Worked exercise on Chapter 6

--- 6.5.py
+++ 6.5.py
@@ -1,3 +1,4 @@
+#Worked exercise for Chapter 6- Strings.
 #Exercise 5: Take the following Python code that stores a string:
 #str = 'X-DSPAM-Confidence:0.8475'
 #Use find and string slicing to extract the portion of the string after the colon character and then use the float function to convert the extracted string into a floating point number.
