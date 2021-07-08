a9cdd044c074951b00c89d458ee69ca9ba0905a0
dpks7 <davidpsoudamalla@gmail.com>
2021-07-07 22:59:30 -0700

    Updated with try and except

--- Determine_pay.py
+++ Determine_pay.py
@@ -1,10 +1,12 @@
-hours = float(input("enter the number of hours: "))
-rate = float(input("enter the rate:"))
-
-#Adding conditional statement to check for different conditions
-if hours <=40:
-    grosspay = hours*rate
-    print ("The pay is:",grosspay)
-else:
-    grosspay=float(((hours-40)*(rate*1.5))+(40*rate))
-    print ("The pay is:",grosspay)
+try: #To catch any misidentified characters
+    hours = float(input("enter the number of hours: "))
+    rate = float(input("enter the rate:"))
+    #Adding conditional statement to check for different conditions
+    if hours <=40:
+        grosspay = hours*rate
+        print ("The pay is:",grosspay)
+    else:
+        grosspay=float(((hours-40)*(rate*1.5))+(40*rate))
+        print ("The pay is:",grosspay)
+except:
+    print("Enter a valid number")
