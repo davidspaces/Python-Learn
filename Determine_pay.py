7bfb862f30493f0ef7fdd28c1899067635ca397a
dpks7 <davidpsoudamalla@gmail.com>
2021-07-07 20:05:48 -0700

    Added conditional statements

--- Determine_pay.py
+++ Determine_pay.py
@@ -1,4 +1,10 @@
 hours = float(input("enter the number of hours: "))
 rate = float(input("enter the rate:"))
-grosspay = hours*rate
-print ("$",grosspay)
+
+#Adding conditional statement to check for different conditions
+if hours <=40:
+    grosspay = hours*rate
+    print ("The pay is:",grosspay)
+else:
+    grosspay=float(((hours-40)*(rate*1.5))+(40*rate))
+    print ("The pay is:",grosspay)
