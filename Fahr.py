e3b623dae6a3815dfc6ccab3903c12ec38502363
dpks7 <davidpsoudamalla@gmail.com>
2021-07-07 19:53:39 -0700

    Added comments for explanation

--- Fahr.py
+++ Fahr.py
@@ -1,5 +1,10 @@
 #Program to convert temperature from Celsius to Fahrenheit
 
 cel = float(input("Enter the temperature in Celsius:"))
+#I'm using float because the temperature can use number with decimal points
 fahr= float((cel*1.8)+32)
+#We are converting the celsius to Fahrenheit using this formula
 print ("It's %.2fF now" %fahr)
+#In this case I want to round the float to 2 decimal places, so I used format function
+#format(0.2f)- for two decimal places or
+#use (%0.2f)- when using string formatting
