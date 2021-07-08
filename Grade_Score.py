242288e0bf02a5ada7ab9fc80159cb2b1078effd
dpks7 <davidpsoudamalla@gmail.com>
2021-07-07 23:13:46 -0700

    Check grades with try and except

---
+++ Grade_Score.py
@@ -0,0 +1,17 @@
+#Program to check the grade
+
+score=float(input("Enter a score between 0.0 and 1.0: "))
+
+try:
+    if score >=0.9 and score <=1.0:
+        print ("A grade")
+    elif score >=0.8 and score<=0.89:
+        print ("B grade")
+    elif score >=0.7 and score<=0.79:
+        print ("C grade")
+    elif score >=0.6 and score<=0.69:
+        print ("D grade")
+    else:
+        print ("F grade")
+except:
+    print ("Bad Score")
